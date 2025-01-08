ea67f30967703f4421ba0eb7c82a72286c637eba
Mehdi Rebiai <mehdi.rebiai.dev@gmail.com>
2025-01-08 14:37:59 +0100

    feat: init repo

---
+++ build-slides.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+ASCIIDOCTOR_DOCKER_IMAGE="asciidoctor/docker-asciidoctor:1.81.0"
+REVEALJS_DIR="https://cdn.jsdelivr.net/npm/reveal.js@5.1.0"
+CONFERENCES=("bdxtestingcommunity")
+
+cat README.adoc > index.adoc
+echo -e "\n== Slides\n" >> index.adoc
+
+rm -rf public
+mkdir -p public
+
+for conf in "${CONFERENCES[@]}"
+do
+  sourceCss=$([[ -f "custom-${conf}.css" ]] && echo "custom-${conf}.css" || echo "custom.css")
+  CONFERENCE_PNG_BASE64=$(cat images/logo-${conf}.png | base64 -w0) \
+  QRCODE_PNG_BASE64=$(cat images/qrcode-slides.png | base64 -w0) \
+    envsubst < ${sourceCss} > public/custom-${conf}.css
+  docker run --name $(uuidgen) --rm -u $(id -u):$(id -g) -v $(pwd):/documents ${ASCIIDOCTOR_DOCKER_IMAGE} \
+    asciidoctor-revealjs -a data-uri -a revealjs_theme=simple \
+    -a conf-${conf} -a confname=${conf} \
+    -a revealjsdir=${REVEALJS_DIR} -a revealjs_transition=fade \
+    -a customcss=custom-${conf}.css -a revealjs_slideNumber=true \
+    -D public -o index-${conf}.html \
+    presentation_en.adoc
+  echo "* link:index-${conf}.html[${conf}^]" >> index.adoc
+done
+
+touch public/.nojekyll
+
+docker run --rm --name $(uuidgen) -u $(id -u):$(id -g) -v $(pwd):/documents ${ASCIIDOCTOR_DOCKER_IMAGE} \
+  asciidoctor -a data-uri -D public -o index.html index.adoc
+
+rm index.adoc
